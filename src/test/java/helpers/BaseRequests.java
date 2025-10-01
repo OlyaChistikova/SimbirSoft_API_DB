c54ddf9b51093f3b6d4bbefcb4b2fd29d150966c
Olya Chistikova <olya.chistikova@mail.ru>
2025-10-01 13:00:26 +0300

    Delete new methods

--- src/test/java/helpers/BaseRequests.java
+++ src/test/java/helpers/BaseRequests.java
@@ -5,14 +5,12 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import pojo.DataError;
 import pojo.DataPost;
-import pojo.DataUser;
 
 import java.util.Base64;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
-
 
 public class BaseRequests {
 
@@ -25,11 +23,6 @@ public class BaseRequests {
      * Путь для взаимодействия с постами.
      */
     public static final String POSTS_PATH = ParametersProvider.getProperty("posts_path");
-
-    /**
-     * Путь для взаимодействия с пользователями.
-     */
-    public static final String USERS_PATH = ParametersProvider.getProperty("users_path");
 
     public static RequestSpecification requestSpec(String authToken) {
         return new RequestSpecBuilder()
@@ -223,7 +216,6 @@ public class BaseRequests {
                 .as(DataError.class);
     }
 
-
     /**
      * Выполняет удаление поста с авторизацией и проверяет, что статус изменился на 'trash'.
      *
@@ -276,7 +268,6 @@ public class BaseRequests {
                 .body("data.status", equalTo(401))
                 .extract().as(DataError.class);
     }
-
 
     /**
      * Успешно обновляет пост по заданным данным.
@@ -338,20 +329,5 @@ public class BaseRequests {
                 .body("data.status", equalTo(401))
                 .extract()
                 .as(DataError.class);
-    }
-
-    /**
-     * Создает объект DataUser с заданным ID и тестовыми данными.
-     *
-     * @param username   Логин пользователя
-     * @param password  Пароль пользователя
-     * @return Объект DataUser с заполненными полями.
-     */
-    public static DataUser createUserBody(String username, String email,  String password) {
-        return DataUser.builder()
-                .username(username)
-                .email(email)
-                .password(password)
-                .build();
     }
 }
